2054e21fc24cd091a2b75c001fc4e74367a85fbf
David Tanzer <business@davidtanzer.net>
2018-04-26 06:53:21 +0200

    Input now renders default value from state

--- src/MyInput.js
+++ src/MyInput.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 export class MyInput extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			inputValue: "Hello"
+		};
+	}
 	render() {
-	  return (
-		<div>
-			<input type="text" value="Hello" />
-			<p>Hello</p>
-		</div>
-	);
+		return (
+			<div>
+				<input type="text" value={this.state.inputValue} />
+				<p>{this.state.inputValue}</p>
+			</div>
+		);
 	}
 }
