70651c50a3150483bc6fe4a8e9bee770b24014a8
David Tanzer <business@davidtanzer.net>
2018-04-26 06:51:09 +0200

    Input renders html text input

--- src/MyInput.js
+++ src/MyInput.js
@@ -3,7 +3,10 @@ import React from 'react';
 export class MyInput extends React.Component {
 	render() {
 	  return (
-		<div>Hello</div>
+		<div>
+			<input type="text" value="Hello" />
+			<p>Hello</p>
+		</div>
 	);
 	}
 }
