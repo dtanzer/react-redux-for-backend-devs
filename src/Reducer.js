f6b4aace0d7bd60f2452f9dcb98bdd6bd3c39887
David Tanzer <business@davidtanzer.net>
2018-04-26 07:13:26 +0200

    Reducer processes action from input

--- src/Reducer.js
+++ src/Reducer.js
@@ -2,9 +2,21 @@ const initialState = {
 	someForm: {
 		inputValue: 'Enter Greeting',
 		otherData: 'We ignore that',
-	}
+	},
+	otherData: 'We ignore that too',
 }
 
 export function reducer(state = initialState, action) {
+	switch(action.type) {
+		case 'GREETING_CHANGED': {
+			return {
+				...state,
+				someForm: {
+					...state.someForm,
+					inputValue: action.newGreeting
+				}
+			}
+		}
+	}
 	return state;
 }
