a4d3e80dab60c09e1f19d0df2626d596ceb7e686
David Tanzer <business@davidtanzer.net>
2018-04-26 06:55:39 +0200

    Input changes state on html text field changed

--- src/MyInput.js
+++ src/MyInput.js
@@ -3,7 +3,7 @@ import React from 'react';
 export class MyInput extends React.Component {
 	constructor(props) {
 		super(props);
-		
+
 		this.state = {
 			inputValue: "Hello"
 		};
@@ -11,9 +11,13 @@ export class MyInput extends React.Component {
 	render() {
 		return (
 			<div>
-				<input type="text" value={this.state.inputValue} />
+				<input type="text" value={this.state.inputValue} onChange={this._inputChanged.bind(this)}/>
 				<p>{this.state.inputValue}</p>
 			</div>
 		);
+	}
+
+	_inputChanged(e) {
+		this.setState({ inputValue: e.target.value });
 	}
 }
